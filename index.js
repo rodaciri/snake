ef9c41915a2eda2ed506d5f4e4eb864ee43f5280
rodaciri <64979495+rodaciri@users.noreply.github.com>
2023-01-24 04:39:50 +0000

    It was added, a full rendering method, a filter of direction method, and the eventlistener to arrows

--- index.js
+++ index.js
@@ -1,28 +1,44 @@
 //If you want to use jQuery, you have to use everything with jQuery.
 var cont = $("#cont");
 
-let size = 8;
+let size = 20;
 cont.html(deployGrid(size));
-cont.css("width",size+"rem");
+cont.css("width", size + "rem");
 
 let a = new Snake(size);
 a.renderSnake();
 
 
+setInterval(() => {
+    a.updatePos();
+    a.renderSnake();
+}, 250);
+
+
 function deployGrid(size) {
     var grid = "";
     for (let index = 0; index < size; index++) {
-        grid += "<div class='row rw-"+index+"'>"+renderCol(size)+"</div>";        
+        grid += "<div class='row rw-" + index + "'>" + renderCol(size) + "</div>";
     }
     return grid;
 }
 function renderCol(size) {
     var cols = "";
     for (let index = 0; index < size; index++) {
-        cols += "<div class='col cl-"+index+"'></div>";        
+        cols += "<div class='col cl-" + index + "'></div>";
     }
     return cols;
 }
+function test() { //this function is to run the game frame by frame
+    a.updatePos();
+    a.renderSnake();
+}
 
+$(document).on("keydown", (event) => {
+        a.updateDir(event.key);
+    })
+
+//This version has renderization of the snake and movement it doesnt have length and bug functions or methods
+//Also is missing a collition detection.
 
 
